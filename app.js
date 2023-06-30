610fa059d04b12dbb8e949154d64c53fe2f4f7d9
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-06-30 06:35:45 -0500

    feat: :sparkles: Config node

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-console.log("xd");
+import {createServer} from "http";
+import https from 'https';
+
+let myServer = createServer((req,res)=>{
+    let data = "";
+    let uri = "https://jsonplaceholder.typicode.com";
+    let key = "/posts";
+    https.get(`${uri}${key}`, (peticion)=>{
+        peticion.on("data", (chunk)=>{
+            data += chunk;
+        })
+        peticion.on("end", ()=>{
+            let obj = JSON.parse(data);
+            let ids = []
+            let title = []
+            obj.forEach((val,id) => {
+                ids[id] = val.id
+                title[id] = val.title
+            });
+            
+            res.end(JSON.stringify(title));
+        });
+    })
+})
+
+/* let myServer = createServer((req,res)=>{
+    let datos = ""
+    req.on("data",(chuck)=>{
+        datos += chuck;
+    })
+    req.on("end", ()=>{
+        res.end(datos);
+    })
+}) */
+
+const config = {
+    hostname: "127.9.012",
+    port: 5016
+}
+
+//LEVANTAR SERVIDOR local = "http://localhost:8080/"
+myServer.listen(config, ()=>{
+    console.log(`http://${config.hostname}:${config.port}/`);
+})
